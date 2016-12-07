db11a5239019aa8ed5afba4cd1149e411f04494a
Lucas Currah <lucas.currah@gmail.com>
2016-12-07 17:18:18 -0500

    Added comments

--- BallClockRepititionCounter/App.xaml.cs
+++ BallClockRepititionCounter/App.xaml.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Ball Clock Simulator
+ * Final Assignment PROG8010, Group 1
+ * Author: Lucas Currah, 7674542
+ * Conestoga College
+ * 
+ * Group members:
+ *      Oleksandr Rudavka
+ *      Jonathan Nagata
+ *      Niral Gandhi
+ *      Tanmay Teckchandani
+ *      Priya Tharuman
+ *      Krishna Kanhaiya
+ *      Lucas Currah
+ * 
+ * 7 December, 2016
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
